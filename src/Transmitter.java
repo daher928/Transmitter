86e53b679164b67a64883f01463cde7b174b2ea3
Daher Daher <40135454+daher928@users.noreply.github.com>
2019-03-10 13:52:04 +0200

    Add files via upload

---
+++ src/Transmitter.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Transmitter {
+	
+	static String fileName = "MW_samples_original_0001613_20181114_1754.txt";
+	static String SOH = String.valueOf((char)1);
+	
+	public static DataSample parseLine(String fullLine) {
+		String line = ((String[])fullLine.split(" "))[1];
+		String[] lineSamples = line.split(SOH);
+		int samplesCount = lineSamples.length;
+		int timeStamp;
+		String sampleId;
+		DataSample dataSample = new DataSample();
+		
+		for (int i=0; i<samplesCount; i++) {
+			String seq = lineSamples[i];
+			if(i==0) {
+				String ts = seq.substring(0, 6);
+				String sc = seq.substring(6,8);
+				timeStamp = Integer.parseInt(ts, 16);
+				if(samplesCount != Integer.parseInt(sc, 16)) {
+					return null;
+				}
+				dataSample.timeStamp = timeStamp;
+				dataSample.samplesCount = samplesCount;
+				String sid = seq.substring(9, 13);
+				String val = seq.substring(13);
+				dataSample.addData(sid, val);
+			}else {
+				String sid = seq.substring(1, 5);
+				String val = seq.substring(6);
+				dataSample.addData(sid, val);
+			}
+		}
+		return dataSample;
+	}
+	
+	public static void main(String[] args) {
+		
+		boolean startData = false;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				if(line.equals("Start DATA")){
+					startData = true;
+					continue;
+				}
+				if(startData) {
+					assert(line.contains(SOH));
+					System.out.println(parseLine(line));
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
